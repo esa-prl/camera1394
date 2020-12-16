6b140e0104f4c48dc5c9ac48220714bda3c41550
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-12-16 17:14:30 +0100

    Add config file

--- launch/camera1394.launch.py
+++ launch/camera1394.launch.py
@@ -1,7 +1,19 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('camera1394'),
+         'config',
+         'marta.yaml'
+    )
+    
+    # Launch declarations
     return LaunchDescription([
         Node(
             package="camera1394",
@@ -10,7 +22,7 @@ def generate_launch_description():
             output="screen",
             emulate_tty=True,
             parameters=[
-                {"guid": "0"}
-            ]
+                config
+            ],
         )
     ])
